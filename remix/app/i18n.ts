4bdf031edd4c4e451f02276cb41470e313333981
多啦b梦 <1715109585@qq.com>
2025-06-02 04:04:19 +0800

    Update i18n.ts

--- remix/app/i18n.ts
+++ remix/app/i18n.ts
@@ -2,17 +2,7 @@ export default {
   // This is the list of languages your application supports
   supportedLngs: [
     "en",
-    "zh",
-    "fr",
-    "ja",
-    "hi",
-    "de",
-    "ko",
-    "zh-TW",
-    "it",
-    "pt",
-    "tr",
-    "ru",
+    "zh"
   ],
   // This is the language you want to use in case
   // if the user language is not in the supportedLngs
