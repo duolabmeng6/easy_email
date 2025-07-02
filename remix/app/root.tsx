fe5978f61970b466a8fc803d29bd004efb555e67
多啦b梦 <1715109585@qq.com>
2025-07-02 14:41:42 +0800

    1

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -48,6 +48,17 @@ export default function App() {
       <body className="">
         <Outlet />
         <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "s8hbez31ld");
+            `,
+          }}
+        />
         <Scripts />
         <LiveReload />
       </body>
